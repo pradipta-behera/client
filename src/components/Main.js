a2158dd7208938d42d4e558200adc8198aa41922
pradipta-behera <pradiptabehera117@gmail.com>
2023-12-13 00:31:07 +0530

    2nd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import image from '../assets/pic1.png';
 
- const Main = () => {
-    return(
-        <div className='main'>
+
+const Main = () => {
+    return (
+       <div className='main'>
             <div className='content'>
                 <h2>Wisdom in Waves:Dive into Quizzes</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nost</p>
                 <button type='button'>Create Quiz</button>
             </div>
             <div className='image'>
-                    <img src={image} alt='front pic' height={400}/>
+                <img src={image} alt='front pic' height={400} />
             </div>
-            
-
-
-        </div>
+        </div> 
     );
- }
- export default Main;
+}
+export default Main;
